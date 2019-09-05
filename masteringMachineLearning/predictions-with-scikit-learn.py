95a18b7b9f07b33a91cd159a60f5275129fee393
Austin Michael Regan <austinmregan@gmail.com>
2019-09-05 18:09:37 -0400

    added more examples of code

--- masteringMachineLearning/predictions-with-scikit-learn.py
+++ masteringMachineLearning/predictions-with-scikit-learn.py
@@ -1,4 +1,6 @@
 # example of training a final regression model
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import make_regression
 # generate regression dataset
@@ -6,3 +8,16 @@ X, y = make_regression(n_samples=100, n_features=2, noise=0.1, random_state=1)
 # fit final model
 model = LinearRegression()
 model.fit(X, y)
+
+
+# example of making a single class prediction
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
+# fit final model
+model = LogisticRegression()
+model.fit(X, y)
+# define one new instance
+Xnew = [[-0.79415228, 2.10495117]]
+# make a prediction
+ynew = model.predict(Xnew)
+print("X=%s, Predicted=%s" % (Xnew[0], ynew[0]))
