e0ef38ed982d44d8de7323c51b6263c0db993199
Austin Michael Regan <austinmregan@gmail.com>
2019-09-05 16:11:18 -0400

    added function to get api data, set up list of records

--- main.py
+++ main.py
@@ -6,11 +6,47 @@ import requests
 import matplotlib.pyplot as plt
 
 
-# Adding an API Key and connecting to DarkSky.net
-# https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+# Adding an API Key and connecting to url
+# https://pudding.cool/2017/12/mars-data/marsWeather.json -> for project
 
-# API_KEY = 'ffd516440a43c75cbd9c4e2a477aedad'
-BASE_URL = "https://pudding.cool/2017/12/mars-data/marsWeather.json"
+API_KEY = '7052ad35e3c73564'
+BASE_URL = "http://api.wunderground.com/api/{}/history_{}/q/NE/Lincoln.json"
 
 
-# https://pudding.cool/2017/12/mars-data/marsWeather.json
+# setting a startdate for year, and creating a list of what the API contains
+target_date = datetime(2016, 5, 16)
+features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",
+            "mintempm", "maxdewptm", "mindewptm", "maxpressurem", "minpressurem", "precipm"]
+DailySummary = namedtuple("DailySummary", features)
+
+
+# getting the data from the api, and creating a list for each day's url
+# for the project, the data will be in one url
+def extract_weather_data(url, api_key, target_date, days):
+    records = []
+    for _ in range(days):
+        request = BASE_URL.format(API_KEY, target_date.strftime('%Y%m%d'))
+        response = requests.get(request)
+        if response.status_code == 200:
+            data = response.json()['history']['dailysummary'][0]
+            records.append(DailySummary(
+                date=target_date,
+                meantempm=data['meantempm'],
+                meandewptm=data['meandewptm'],
+                meanpressurem=data['meanpressurem'],
+                maxhumidity=data['maxhumidity'],
+                minhumidity=data['minhumidity'],
+                maxtempm=data['maxtempm'],
+                mintempm=data['mintempm'],
+                maxdewptm=data['maxdewptm'],
+                mindewptm=data['mindewptm'],
+                maxpressurem=data['maxpressurem'],
+                minpressurem=data['minpressurem'],
+                precipm=data['precipm']))
+        time.sleep(6)
+        target_date += timedelta(days=1)
+    return records
+
+
+# creating a list of records, for the past 100 days
+records = extract_weather_data(BASE_URL, API_KEY, target_date, 100)
